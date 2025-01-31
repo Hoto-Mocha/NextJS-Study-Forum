2246ef9fa0357f2c0d564ee920ab2f62f1b5561a
Hoto-Mocha <mtj2010@naver.com>
2025-01-31 16:54:07 +0900

    데이터베이스 연결 예제 구현

--- app/page.js
+++ app/page.js
@@ -1,7 +1,15 @@
-export default function Home() {
-  return (
-    <div>
-      
-    </div>
-  );
+import { connectDB } from "@/util/database";
+
+export default async function Home() {
+
+    const db = (await connectDB).db("forum")
+
+    let result = await db.collection('post').find().toArray()
+    console.log(result)
+
+    return (
+        <div>
+            안녕
+        </div>
+    );
 }
