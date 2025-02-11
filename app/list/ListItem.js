f1cc4fb99940d7db824a9215d55b8fe4c7d60db9
Hoto-Mocha <mtj2010@naver.com>
2025-02-11 16:21:09 +0900

    게시물 삭제 애니메이션 예제 구현

--- app/list/ListItem.js
+++ app/list/ListItem.js
@@ -16,13 +16,27 @@ export default function ListItem(props) {
                             </Link>
                             <p>1월 1일</p>
                             <Link href={`/edit/${id}`}>✏️</Link>
-                            <span onClick={() => {
-                                fetch('/api/delete', {
-                                    method:'POST',
-                                    body:JSON.stringify({contentId: id})
-                                }).then(() => {
-                                    
+                            <span onClick={(e) => {
+                                fetch(`/api/delete?contentId=${id}`, {
+                                    method:'DELETE'
                                 })
+                                .then((r)=>{
+                                    if(r.status == 200) {
+                                      return r.json()
+                                    } else {
+                                      //서버가 에러코드전송시 실행할코드
+                                    }
+                                  })
+                                  .then((result)=>{ 
+                                    //성공시 실행할코드
+                                    e.target.parentElement.style.opacity = 0;
+                                    setTimeout(() => {
+                                        e.target.parentElement.style.display = 'none';
+                                    }, 1000)
+                                  }).catch((error)=>{
+                                    //인터넷문제 등으로 실패시 실행할코드
+                                    console.log(error)
+                                  })
                             }}>🗑️</span>
                         </div>
                     )
--- pages/api/delete.js
+++ pages/api/delete.js
@@ -2,14 +2,36 @@ import { connectDB } from "@/util/database"
 import { ObjectId } from "mongodb"
 
 export default async function handler(req, res) {
-    if (req.method == 'POST') {
-        let contentId = JSON.parse(req.body).contentId
-        console.log(contentId)
-        const db = (await connectDB).db("forum")
-        // let result = await db.collection('post').findOne({_id: new ObjectId(contentId)})
-        // console.log(result)
-        await db.collection('post').deleteOne({_id: new ObjectId(contentId)})
+    if (req.method == 'DELETE') {
+        // try {
+        //     let contentId = JSON.parse(req.body).contentId
+        //     const db = (await connectDB).db("forum")
+        //     let result = await db.collection('post').deleteOne({ _id: new ObjectId(contentId) })
+        //     console.log(result)
+        //     if (result.deletedCount > 0) {
+        //         return res.status(200).json('삭제완료')
+        //     } else {
+        //         return res.status(500).json('게시글을 찾을 수 없음')
+        //     }
+        // } catch (error) {
+        //     console.log(error)
+        //     return res.status(500).json('DB 에러')
+        // }
 
-        return res.status(200).json('')
+        // console.log(req.query)
+        try {
+            let contentId = req.query.contentId
+            const db = (await connectDB).db("forum")
+            let result = await db.collection('post').deleteOne({ _id: new ObjectId(contentId) })
+            console.log(result)
+            if (result.deletedCount > 0) {
+                return res.status(200).json('삭제완료')
+            } else {
+                return res.status(500).json('게시글을 찾을 수 없음')
+            }
+        } catch (error) {
+            console.log(error)
+            return res.status(500).json('DB 에러')
+        }
     }
 }
