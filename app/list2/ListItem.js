63005073e51be68aff0e09ff3ff40f21bccd54dc
Hoto-Mocha <mtj2010@naver.com>
2025-02-26 13:59:27 +0900

    dynamic rendering 예제 구현

---
+++ app/list2/ListItem.js
@@ -0,0 +1,47 @@
+'use client'
+
+import Link from "next/link";
+
+export default function ListItem(props) {
+    let result = JSON.parse(props.result)
+    return (
+        <div>
+            {
+                result.map((a, i) => {
+                    let id = a._id.toString();
+                    return (
+                        <div className="list-item" key={i}>
+                            <Link href={`/detail/${id}`}>
+                                <h4>{a.title}</h4>
+                            </Link>
+                            <p>1월 1일</p>
+                            <Link href={`/edit/${id}`}>✏️</Link>
+                            <span onClick={(e) => {
+                                fetch(`/api/delete?contentId=${id}`, {
+                                    method:'DELETE'
+                                })
+                                .then((r)=>{
+                                    if(r.status == 200) {
+                                      return r.json()
+                                    } else {
+                                      //서버가 에러코드전송시 실행할코드
+                                    }
+                                  })
+                                  .then((result)=>{ 
+                                    //성공시 실행할코드
+                                    e.target.parentElement.style.opacity = 0;
+                                    setTimeout(() => {
+                                        e.target.parentElement.style.display = 'none';
+                                    }, 1000)
+                                  }).catch((error)=>{
+                                    //인터넷문제 등으로 실패시 실행할코드
+                                    console.log(error)
+                                  })
+                            }}>🗑️</span>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -2,10 +2,10 @@ import { connectDB } from "@/util/database";
 
 export default async function Home() {
 
-    const db = (await connectDB).db("forum")
+    // const db = (await connectDB).db("forum")
 
-    let result = await db.collection('post').find().toArray()
-    console.log(result)
+    // let result = await db.collection('post').find().toArray()
+    // console.log(result)
 
     return (
         <div>
