b80e542df5658dd73e76eb2e02c0642eccf7ba83
Hoto-Mocha <mtj2010@naver.com>
2025-03-03 11:57:17 +0900

    글쓰기 시 글에 작성자의 이메일도 같이 저장하도록 수정

--- pages/api/write.js
+++ pages/api/write.js
@@ -1,21 +1,20 @@
 import { connectDB } from "@/util/database"
+import { getServerSession } from "next-auth"
+import { authOptions } from "./auth/[...nextauth]"
 
 export default async function handler(req, res) {
-    if (req.method == 'POST') {
-        console.log(req.body)
-        if (req.body.title == '') {
-            return res.status(500).json('너 왜 제목 안씀')
+    let session = await getServerSession(req, res, authOptions)
+    if (session) {
+        req.body.auther = session.user.email
+        
+        if (req.method == 'POST') {
+            console.log(req.body)
+            if (req.body.title == '') {
+                return res.status(500).json('너 왜 제목 안씀')
+            }
+            const db = (await connectDB).db("forum")
+            await db.collection('post').insertOne(req.body)
+            return res.redirect(302, '/list')
         }
-        // try {
-        //     const db = (await connectDB).db("forum")
-        //     await db.collection('post').insertOne(req.body)
-        //     return res.redirect(302, '/list')
-        // } catch (error) {
-        //     console.log(error)
-        //     return res.status(500).json(error)
-        // }
-        const db = (await connectDB).db("forum")
-        await db.collection('post').insertOne(req.body)
-        return res.redirect(302, '/list')
     }
 }
